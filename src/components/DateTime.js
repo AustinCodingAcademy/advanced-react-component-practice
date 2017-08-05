c9bb115edd599260528af7c744371fea1fb4d41e
mymonstermaggie <maggie@remote-com.com>
2017-08-05 16:05:08 -0500

    components created

---
+++ src/components/DateTime.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DateTime() {
+  return (
+    <p className="small text-muted">
+      <i className="fa fa-clock-o" />
+      Yesterday at 4:32 PM
+    </p>
+  );
+}
+
+export default DateTime;
