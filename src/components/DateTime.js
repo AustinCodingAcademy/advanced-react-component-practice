6a11f3a51b1fead38e2f794f72af2825faa00958
Jordon R <reese.jordon@gmail.com>
2017-10-18 20:55:01 -0500

    Added connection of all components

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 function DateTime() {
-  <div id="DateTime">
-    <p className="small text-muted"><i className="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-  </div>
+  return(
+    <div id="DateTime">
+      <p className="small text-muted"><i className="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
+    </div>
+  )  
 }
 export default DateTime;
